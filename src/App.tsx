24d669d9909f49e9a409331f88b60e71001749fd
gero <geronicola1696@gmail.com>
2025-08-30 03:05:41 -0300

    arreglo

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ import './css/detalleProducto.css'
 import './css/react-styles.css'
 
 // Inicializar MercadoPago con la public key y configuración en español
-initMercadoPago(MERCADOPAGO_CONFIG.PUBLIC_KEY, { locale: 'es' })
+initMercadoPago(MERCADOPAGO_CONFIG.PUBLIC_KEY)
 
 function App() {
   return (
