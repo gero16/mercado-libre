2e84e31301bbfc8150e7a70dd5147ca7517dcc24
gero <geronicola1696@gmail.com>
2025-10-09 13:02:56 -0300

    arreglitos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -32,11 +32,11 @@ const Footer: React.FC = () => {
           <div className="redes-sociales">
             <a href="https://instagram.com/poppyshopuy" target="_blank" rel="noopener noreferrer" className="social-link">
               <img src="https://res.cloudinary.com/geronicola/image/upload/v1759325974/dodgeball/adh6vtxq60nmkhunw9n9.png" alt="logo-instagram" />
-              <span>@poppyshopuy</span>
+              <span>@poppyshop.uy</span>
             </a>
             <a href="https://facebook.com/poppyshopuy" target="_blank" rel="noopener noreferrer" className="social-link">
               <img src="https://res.cloudinary.com/geronicola/image/upload/v1759325974/dodgeball/yvzfdz5pyj6fybkv9p96.png" alt="logo-facebook" />
-              <span>Poppy Shop Uruguay</span>
+              <span>Poppy Shop </span>
             </a>
             <a href="https://wa.me/59892701630?text=%C2%A1Hola!%Poppy%20Shop!%Tengo%una%consulta...%" target="_blank" rel="noopener noreferrer" className="social-link">
               <img src="/img/whatsapp.png" alt="logo-whatsapp" />
