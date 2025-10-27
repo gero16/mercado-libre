2d980db64758c151b406c3d0ed8fb5096a675693
gero <geronicola1696@gmail.com>
2025-10-27 17:04:44 -0300

    dolares

--- src/config/featureFlags.ts
+++ src/config/featureFlags.ts
@@ -3,6 +3,6 @@ export const FEATURE_FLAGS = {
   USE_CHECKOUT_PRO_SANDBOX: false, // usar init_point en producción
   USE_CHECKOUT_PRO: true, // activar flujo de Checkout Pro en el frontend
   // Temporal: manejar moneda en UYU en frontend y payloads
-  PRICE_IN_UYU: true,
+  PRICE_IN_UYU: false,
   PRICE_CONVERSION_RATE: 1, // multiplica montos (ej: 40 si tus precios base están en USD)
 }
