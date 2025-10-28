e66fab15ef6dd91026f72ccacfe98511f485ae89
gero <geronicola1696@gmail.com>
2025-10-28 01:41:39 -0300

    arrelgos visuales en footer mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,17 +5,7 @@ const Footer: React.FC = () => {
   return (
     <footer>
       <div className="container">
-        <div className="pagos">
-          <ul className="lista-pagos">
-            <li><img className="img-pagos" src="/img/pagos/mercadopago.svg" alt="Mercado Pago" /></li>
-            <li><img className="img-pagos" src="/img/pagos/redpagos.svg" alt="Red Pagos" /></li>
-            <li><img className="img-pagos" src="/img/pagos/abitab.svg" alt="Abitab" /></li>
-            <li><img className="img-pagos" src="/img/pagos/oca.svg" alt="OCA" /></li>
-            <li><img className="img-pagos" src="/img/pagos/visa.svg" alt="Visa" /></li>
-            <li><img className="img-pagos" src="/img/pagos/master.svg" alt="Mastercard" /></li>
-          </ul>
-        </div>
-
+      
         <div className="footer">
           <p>© Copyright 2025 / Poppy Shop Uruguay </p>
        
